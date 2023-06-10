f6d5971a84f57bda797a2d5a63fd626bd0bc925d
EOussama <oussama_gtacj@live.fr>
2023-06-10 21:29:41 +0100

    🔢 Refactored Files (#3)

--- src/file.c
+++ src/file.c
@@ -1,4 +1,8 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "debug.h"
 #include "binary.h"
 
 void removeExtension(const char *fileName, char *result)
@@ -20,4 +24,40 @@ void removeExtension(const char *fileName, char *result)
     // If no dot found, return the original file name
     strcpy(result, fileName);
   }
+}
+
+unsigned char *readFile(const char *fileName, long *size)
+{
+  debug("File - Opening file '%s'...", fileName);
+
+  // Opening the file
+  FILE *file = fopen(fileName, "rb");
+  if (file == NULL)
+  {
+    debug("[Error] File - Error opening the file");
+    return NULL;
+  }
+
+  // Getting the content's length
+  fseek(file, 0, SEEK_END);
+  *size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  // Allocating memory to dump the contents to
+  unsigned char *buffer = (unsigned char *)malloc(*size * sizeof(unsigned char));
+  if (buffer == NULL)
+  {
+    debug("[Error] File - Memory allocation failed");
+    fclose(file);
+    return NULL;
+  }
+
+  // Storing the file's content into memory
+  fread(buffer, sizeof(unsigned char), *size, file);
+  fclose(file);
+
+  debug("File - File opened");
+  debug("File - Buffer length %d", strlen((char *)buffer));
+
+  return buffer;
 }
--- src/file.h
+++ src/file.h
@@ -2,5 +2,6 @@
 #define FILE_UTILS_H
 
 void removeExtension(const char* fileName, char* result);
+unsigned char *readFile(const char *fileName, long *size);
 
 #endif
